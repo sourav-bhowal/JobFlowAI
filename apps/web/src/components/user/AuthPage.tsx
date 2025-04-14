ea8b1cb020c21a1f17115552e0f9d24e65d42147
Sourav Bhowal <souravbhowal15@gmail.com>
2025-04-14 22:28:47 +0530

    stage 10

--- apps/web/src/components/user/AuthPage.tsx
+++ apps/web/src/components/user/AuthPage.tsx
@@ -80,7 +80,7 @@ export default function AuthPage({ isSignIn }: AuthPageProps) {
           }
         } else {
           toast.success("Signed in successfully");
-          router.replace("/user/profile");
+          router.refresh();
         }
       });
     } else {
--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -16,10 +16,9 @@ export default auth((req) => {
   if (
     req.auth &&
     (req.nextUrl.pathname.startsWith("/signin") ||
-      req.nextUrl.pathname.startsWith("/signup") ||
-      req.nextUrl.pathname.startsWith("/"))
+      req.nextUrl.pathname.startsWith("/signup"))
   ) {
-    const newUrl = new URL("/user/profile", req.nextUrl.origin);
+    const newUrl = new URL("/", req.nextUrl.origin);
     return Response.redirect(newUrl);
   }
 });
