33fab69e51e4e16d146336e6aa8478828f1dbb94
Sourav Bhowal <souravbhowal15@gmail.com>
2025-04-26 02:04:23 +0530

    docker

--- apps/server/index.ts
+++ apps/server/index.ts
@@ -33,7 +33,7 @@ consumeJobsFromQueue()
   });
 
 // Cron job to scrape jobs every day at 12:00 AM
-cron.schedule("0 2 * * *", async () => {
+cron.schedule("15 2 * * *", async () => {
   try {
     console.log("Scraping jobs...");
 
