880c4171da75c4df3bd982e9571d5f9afb74e8fc
Sourav Bhowal <souravbhowal15@gmail.com>
2025-04-28 16:37:05 +0530

    vercel cron

--- apps/web/src/app/api/crons/delete-jobs/route.ts
+++ apps/web/src/app/api/crons/delete-jobs/route.ts
@@ -1,7 +1,7 @@
 import { db, lt } from "@repo/db/drizzle";
 import { jobs } from "@repo/db/schema";
 
-// Delete all jobs from the database
+// Delete jobs older than 10 days from the database
 export async function GET(request: Request) {
   try {
     // Headers check
@@ -16,17 +16,17 @@ export async function GET(request: Request) {
     }
 
     // Get the current date and time
-    const oneMonthAgo = new Date();
+    const tenDaysAgo = new Date();
 
-    // Set the date to one month ago
-    oneMonthAgo.setMonth(oneMonthAgo.getMonth() - 1);
+    // Subtract 10 days
+    tenDaysAgo.setDate(tenDaysAgo.getDate() - 10);
 
-    // Delete all jobs older than one month
-    await db.delete(jobs).where(lt(jobs.createdAt, oneMonthAgo));
+    // Delete all jobs older than 10 days
+    await db.delete(jobs).where(lt(jobs.createdAt, tenDaysAgo));
 
-    //  Return success response
+    // Return success response
     return Response.json(
-      { message: "Successfully deleted old jobs from the database" },
+      { message: "Successfully deleted jobs older than 10 days" },
       { status: 200 }
     );
   } catch (error) {
