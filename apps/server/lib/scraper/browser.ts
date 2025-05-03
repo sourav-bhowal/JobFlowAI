d2959ecc9f2be378cedc2fc7a1f253bbc91a3014
Sourav Bhowal <souravbhowal15@gmail.com>
2025-05-04 02:11:54 +0530

    fixes few

--- apps/server/lib/scraper/browser.ts
+++ apps/server/lib/scraper/browser.ts
@@ -3,7 +3,7 @@ import puppeteer from "puppeteer";
 // This function is used to launch a headless browser using Puppeteer.
 export const getBrowser = async () => {
   const browser = await puppeteer.launch({
-    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    args: ["--no-sandbox", "--disable-setuid-sandbox", "--disable-http2"],
     headless: true,
   });
 
