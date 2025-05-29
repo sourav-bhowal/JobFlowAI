b5dc5ef8f71b117a6e0bdae8d2d6b98cb5ac9aee
Sourav Bhowal <souravbhowal15@gmail.com>
2025-05-29 23:15:35 +0530

    aws changed

--- apps/server/lib/scraper/browser.ts
+++ apps/server/lib/scraper/browser.ts
@@ -1,10 +1,15 @@
-import puppeteer from "puppeteer";
+import puppeteer from "puppeteer-extra";
+import StealthPlugin from "puppeteer-extra-plugin-stealth";
+
+// @ts-ignore
+puppeteer.use(StealthPlugin());
 
 // This function is used to launch a headless browser using Puppeteer.
 export const getBrowser = async () => {
+  // @ts-ignore
   const browser = await puppeteer.launch({
     args: ["--no-sandbox", "--disable-setuid-sandbox", "--disable-http2"],
-    headless: true,
+    headless: false,
   });
 
   return browser;
