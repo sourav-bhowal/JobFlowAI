3dea169ea87d99125dd988fe6d36c95680dc221e
Sourav Bhowal <souravbhowal15@gmail.com>
2025-07-12 13:08:04 +0530

    fe

--- apps/web/src/components/home/Hero.tsx
+++ apps/web/src/components/home/Hero.tsx
@@ -5,7 +5,7 @@ import HeroTiles from "./HeroTiles";
 // Hero Section
 export default function Hero() {
   return (
-    <section className="relative flex min-h-screen flex-col items-center justify-center bg-neutral-950 px-4 pt-24 max-sm:pt-32 max-sm:pb-20 text-center text-white overflow-hidden">
+    <section className="relative flex min-h-screen flex-col items-center justify-center bg-neutral-950 px-4 pt-20 max-sm:pt-32 max-sm:pb-20 text-center text-white overflow-hidden">
       <div className="pointer-events-none absolute -top-20 -left-20 h-[400px] w-[400px] rounded-full bg-orange-500 opacity-10 blur-[120px]"></div>
       <div className="pointer-events-none absolute bottom-0 right-0 h-[500px] w-[500px] rounded-full bg-orange-400 opacity-10 blur-[160px]"></div>
       <div className="pointer-events-none absolute top-1/3 left-1/2 h-[300px] w-[300px] -translate-x-1/2 -translate-y-1/2 rounded-full bg-white opacity-5 blur-[200px]"></div>
@@ -15,7 +15,7 @@ export default function Hero() {
           <div className="mb-4 inline-block rounded-full bg-gradient-to-r from-yellow-600/10 to-orange-600/10 px-3 py-1 text-sm text-orange-500">
             {/* Pulsating dot */}
             <span className="inline-block h-2 w-2 animate-pulse mr-1 rounded-full bg-orange-500"></span>
-            Early Access
+            YC Jobs Added
           </div>
         </div>
 
@@ -32,7 +32,7 @@ export default function Hero() {
         </div>
 
         {/* Yellow underline */}
-        <div className="mx-auto mb-10 h-1 w-20 bg-gradient-to-r from-yellow-600 to-orange-600"></div>
+        <div className="mx-auto mb-6 h-1 w-20 bg-gradient-to-r from-yellow-600 to-orange-600"></div>
 
         {/* Main headline */}
         <h2 className="mb-8 text-4xl font-bold leading-tight md:text-5xl">
@@ -69,7 +69,7 @@ export default function Hero() {
       </div>
 
       {/* Down arrow */}
-      <div className="absolute bottom-3 animate-bounce text-yellow-600">
+      <div className=" animate-bounce text-yellow-600 absolute bottom-2">
         <ChevronDown size={32} />
       </div>
     </section>
